91e79c6e31cd035cc942ec4b50cde1271496b2e0
Your Name <you@example.com>
2025-05-13 15:32:04 -0700

    fixes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const contactsRoutes = require('./routes/contacts');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
 app.use(express.json());
 app.use('/contacts', contactsRoutes);
 
