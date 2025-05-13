2420d7bdfabcdd0c692ee06fdb8ab9685cb35ddb
Your Name <you@example.com>
2025-05-13 15:47:32 -0700

    fixes

--- server.js
+++ server.js
@@ -6,20 +6,17 @@ const contactsRoutes = require('./routes/contacts');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' folder
-app.use(express.static('public'));
-
+// Middleware
+app.use(express.static('public')); // Serve public/index.html, styles.css, etc.
 app.use(express.json());
 app.use('/contacts', contactsRoutes);
 
+// DB Connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
-//app.get('/', (req, res) => {
-  //res.send('CSE341 Contacts API Running');
-//});
-
+// Start Server
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
