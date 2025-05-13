58cbade91afe5870e658ef738a58d724d96f5870
Your Name <you@example.com>
2025-05-13 15:45:04 -0700

    fixes

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-  res.send('CSE341 Contacts API Running');
-});
+//app.get('/', (req, res) => {
+  //res.send('CSE341 Contacts API Running');
+//});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
