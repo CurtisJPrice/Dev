38d0fd94a51c7c5a932c19a99d035ab1e9b806d3
Your Name <you@example.com>
2025-05-14 15:52:44 -0700

    fixes

--- server.js
+++ server.js
@@ -26,11 +26,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+// MongoDB Connection (clean, no deprecated options)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected successfully');
     mongoose.connection.db.listCollections().toArray((err, collections) => {
