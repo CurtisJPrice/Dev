7b307be3c60c8aa1f827b63370a0972d97b7517b
Your Name <you@example.com>
2025-05-17 02:40:30 -0700

    fix

--- test-server.js
+++ test-server.js
@@ -3,4 +3,4 @@ const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200);
   res.end('Hello from Node!');
-}).listen(3000, () => console.log('🧪 Test server on port 3000'));
+}).listen(3001, () => console.log('🧪 Test server on port 3001'));
